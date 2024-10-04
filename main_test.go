c8e81ee50e123985dfb5c14ebc500bfb3855d61a
SundayPc <46506560@qq.com>
2024-10-04 22:38:48 +0800

    commite 02

--- main_test.go
+++ main_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestTaskControl(t *testing.T) {
+	taskNum := 5
+	wg := sync.WaitGroup{}
+	wg.Add(taskNum)
+
+	for i := 0; i < taskNum; i++ {
+		go func(i int) {
+			fmt.Println("info", i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
